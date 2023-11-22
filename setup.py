41d2f9fdc09080fd4c4a4f3799a00f67b2634a9f
Julio César Martínez Sánchez <53662692+JulioCesarMS@users.noreply.github.com>
2023-11-21 21:07:34 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from setuptools import setup, find_packages
+import io
+import os
 
 
-setup (
-    author= "Julio César Martínez",
-    description="A package to simulate stochastic proccesses",
-    name='stochasticmodels',
-    version='0.1.0',
-    packages=find_packages(include=["numpy", "pandas", ""]),
-    ),
+def read(file_name):
+    """Read a text file and return the content as a string."""
+    with io.open(os.path.join(os.path.dirname(__file__), file_name),
+                 encoding='utf-8') as f:
+        return f.read()
 
+setup(
+    name='stochasticprocesses',
+    description='A package to simulate stochastic processes',
+    author='Julio César Martínez',
+    author_email='julio.martinez@gmail.com.mx',
+    version='0.15.0',
+    long_description=read('Readme.md'),
+    long_description_content_type='text/markdown',
+    #packages=['stochasticprocesses'],
+    packages=find_packages(),
+    install_requires=["pandas >= 1.0.0", "numpy >= 1.15.0"],
+    python_requires=">= 3.7.3",
+    url='https://github.com/JulioCesarMS/StochasticModels.git',
+    
+
+)
 
 # pip install -e .
